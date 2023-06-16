e09f341922242129dbcfac9c70d3eb453ce266bd
PVSKR <skr.polu@hotmail.com>
2023-06-16 22:51:10 +0530

    few links add to the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Name of the site: <span style={{color:'Green'}}>SRAVANPOLU.ME</span></h1>
+    <h3>I am going to make beautiful <span style={{color:'red'}}>web site</span></h3>
+      
     </div>
   );
 }
