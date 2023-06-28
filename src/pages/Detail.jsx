8cbb63d860aa54fff09f2c54cd06970b32200391
PVSKR <skr.polu@hotmail.com>
2023-06-28 22:57:49 +0530

    detail-resume-main  updating.

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -4,7 +4,48 @@ export default function Detail() {
   return (
     <div className="detail-resume-grid">
       <main className="detail-resume-main">
-        <h1>MyResume</h1>
+        <h2>Courses</h2>
+        <p className="intro">
+          Desire! That’s the one secret of every man’s career. Not education.
+          Not being born with hidden talents. Desire.
+        </p>
+        <div className="course-experience">
+          <div className="course">
+            <p className="courseName">CSS</p>
+            <div className="job-info">
+              <h2 className="role">projects</h2>
+              <span className="company"> BCDPROJECT</span>
+            </div>
+          </div>
+          <div className="course">
+            <p className="courseName">JS</p>
+            <div className="job-info">
+              <h2 className="role">projects</h2>
+              <span className="company"> BCDPROJECT</span>
+            </div>
+          </div>
+          <div className="course">
+            <p className="courseName">HTML</p>
+            <div className="job-info">
+              <h2 className="role">projects</h2>
+              <span className="company"> BCDPROJECT</span>
+            </div>
+          </div>
+          <div className="course">
+            <p className="courseName">React</p>
+            <div className="job-info">
+              <h2 className="role">projects</h2>
+              <span className="company"> BCDPROJECT</span>
+            </div>
+          </div>
+          <div className="course">
+            <p className="courseName">BootStap</p>
+            <div className="job-info">
+              <h2 className="role">projects</h2>
+              <span className="company"> BCDPROJECT</span>
+            </div>
+          </div>
+        </div>
       </main>
       <aside className="detail-resume-aside">
         <h2 className="visually-hidden">myskills</h2>
@@ -27,7 +68,7 @@ export default function Detail() {
           </li>
           <li>
             <p className="visually-hidden"></p>
-            <img src="/svg/scss.svg"></img>
+            <img src="/svg/bootstrap.svg"></img>
           </li>
         </ul>
       </aside>
