53fd16d680ad451f0f3923d4ee2a1fb940291425
PVSKR <skr.polu@hotmail.com>
2023-07-03 21:32:59 +0530

    Form is creating with help of Form hook in react

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,69 @@
 import React from "react";
-import { Form, useSubmit } from "react-router-dom";
+import { loginUser } from "../api";
+import {
+  redirect,
+  useLoaderData,
+  useNavigate,
+  Form,
+  useActionData,
+} from "react-router-dom";
+
+export function loader({ request }) {
+  return new URL(request.url).searchParams.get("message");
+}
+export async function action({ request }) {
+  const formData = await request.formData();
+  const password = formData.get("password");
+  const email = formData.get("email");
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem("loggedin", true);
+
+    return redirect("/host");
+  } catch (err) {
+    return err.message;
+  }
+}
+
 export default function Login() {
   const [loginFormData, setLoginFormData] = React.useState({
     email: "",
     password: "",
   });
+  const [status, setStatus] = React.useState("idle");
+  const [error, setError] = React.useState(null);
+  const errorMessage = useActionData();
+  const message = useLoaderData();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    setStatus("submitting");
+    setError(null);
+    loginUser(loginFormData)
+      .then((data) => {
+        navigate("/about", { replace: true });
+      })
+      // .catch(err=>setError(err))
+      .finally(() => setStatus("idle"));
+    console.log(loginFormData);
   }
+
   function handleChange(e) {
-    const { name, value } = e.targer;
+    const { name, value } = e.target; // Fix typo from 'e.tartget' to 'e.target'
     setLoginFormData((prev) => ({
       ...prev,
       [name]: value,
     }));
   }
+
   return (
     <div className="login-container">
       <h1>Sing in to your account</h1>
-
-      <form
-        method="post"
-        on
-        onSubmit={handleSubmit}
-        className="login-form"
-        replace
-      >
+      {message && <h2 className="red">{message}</h2>}
+      {/* {error && <h4 className="red">{error.message}</h4>} */}
+      {errorMessage && <h4 className="red">{errorMessage}</h4>}
+      <Form method="post" className="login-form" replace>
         <input
           name="email"
           onChange={handleChange}
@@ -41,8 +78,10 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Login</button>
-      </form>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "LogIng in ..." : "Log in"}
+        </button>
+      </Form>
     </div>
   );
 }
